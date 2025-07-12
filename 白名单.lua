f940b05d708e320a63b4944324b674d1147b27e4
SUNXIAOCHUAN886 <3598439173@qq.com>
2025-07-12 18:21:35 +0800

    Add files via upload

---
+++ 白名单.lua
@@ -0,0 +1,23 @@
+repeat
+    task.wait()
+until game.Players.LocalPlayer.Character~=nil;
+local a=tostring(game.Players.LocalPlayer.Character);
+if a=="woainiyibeizi888"then
+_G.whitelisted=true
+elseif a=="sjqgduf6"then
+_G.whitelisted=true
+elseif a=="bbbibbj"then
+_G.whitelisted=true
+elseif a=="jjb1169"then
+_G.whitelisted=true
+elseif a=="ksxw666"then
+_G.whitelisted=true
+elseif a=="mnlewiskv171"then
+_G.whitelisted=true
+end
+if _G.whitelisted==true then
+loadstring(game:HttpGet("https://pastebin.com/raw/DNHCLyBB"))()
+    else
+ 
+game.Players.LocalPlayer:Kick(a.."你没买白名单😭求你买一个吧,群号981779561,找SXC")
+end
